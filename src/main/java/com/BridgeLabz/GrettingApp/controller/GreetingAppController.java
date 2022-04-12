4e2070af1c540ea83d8c1b3021aa44d0d57b3fc3
pankaj kore <korep95@gmail.com>
2022-04-12 23:27:57 +0530

    [pankaj]added using controller return json

--- src/main/java/com/BridgeLabz/GrettingApp/controller/GreetingAppController.java
+++ src/main/java/com/BridgeLabz/GrettingApp/controller/GreetingAppController.java
@@ -1,8 +1,18 @@
 package com.BridgeLabz.GrettingApp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import com.BridgeLabz.GrettingApp.Entity.Greeting;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingAppController {
+    private static final String template="Hello,%s";
+    private final AtomicLong counter=new AtomicLong();
 
+    @GetMapping("Greeting")
+    public Greeting greeting(@RequestParam(value="name",defaultValue = "World")String name){
+        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
 }
